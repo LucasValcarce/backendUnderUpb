1e5058a48c28bb69302d606fbbc269ebe06972d0
lucas <lucasvalcarce03@gmail.com>
2025-12-14 01:45:57 -0400

    remover servicio innecesario

--- backendUnderUpb/src/main/java/com/UnderUpb/backendUnderUpb/application/service/GameplayService.java
+++
@@ -1,8 +0,0 @@
-package com.UnderUpb.backendUnderUpb.application.service;
-
-import com.UnderUpb.backendUnderUpb.dto.gameplay.LevelCompletionDto;
-import com.UnderUpb.backendUnderUpb.dto.gameplay.LevelCompletionResponseDto;
-
-public interface GameplayService {
-    LevelCompletionResponseDto completeLevel(LevelCompletionDto completionDto);
-}
